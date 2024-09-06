2bb44bf7b9c4e8a6d9e8485ea5c0aa99550e5c64
you0626 <sy0626sk@icloud.com>
2024-09-06 23:30:04 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, Only: [:edit, :update]
+  resources :rooms, Only: [:new, :create]
 end
